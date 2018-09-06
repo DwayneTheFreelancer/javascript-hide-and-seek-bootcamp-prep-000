d4f24b277aa62483a684f0f96ebf043f7054e703
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-06 03:16:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function getFirstSelector(selector) {
+  return selector[0];
+}
+
+function nestedTarget() {
+  const lis = document.getElementById("nested").querySelector(".target");
+  return lis;
+}
+
+function increaseRankBy(n) {
+  n = document.getElementById("main").querySelectorAll("ranked-list");
+  for (let i = 0; i < n.length; i++) {
+    n[i].innerHTML = (i + 1).parseInt();
+  }
+  return n;
+}
