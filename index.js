9a011ffa422cc8be29bf0520a3f4aa2adf493cdb
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-08 03:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+<<<<<<< HEAD
   var li = document.getElementById("app").querySelectorAll(".ranked-list li");
   for (let i = 0; i < li.length; i++) {
     li[i].innerHTML = parseInt(li[i].innerHTML) + n;
+=======
+  n = document.getElementById("app").querySelectorAll(".ranked-list li");
+  for (let i = 0; i < n.length; i++) {
+    n[i].innerHTML = (n[i] + 1).parseInt();
+>>>>>>> bea77f3907ef49b44fe40a6a28ebffbf8cca8f36
   }
   return n;
 }
